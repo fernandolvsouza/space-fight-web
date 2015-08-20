f86ed65029a659d24f1b3d9c1c3ac76944c0233e
Fernando Valente <uq4n@petrobras.com.br>
2015-08-20 19:28:51 -0300

    tiro com particulas

--- game.js
+++ game.js
@@ -2,6 +2,7 @@
 var express = require("express")
 var app = express()
 app.use(express.static(__dirname + "/static/"))
+app.use(express.static(__dirname + "/"))
 app.use(express.static(__dirname + "/node_modules/bootstrap/dist/"))
 
 var router = require("./router.js");
